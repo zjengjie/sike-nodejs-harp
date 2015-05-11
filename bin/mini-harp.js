707db2cff9105cb319c6d3f13f23efffbb148910
Zeng Jie <zjengjie@gmail.com>
2015-05-12 02:26:29 +0800

    time server

--- bin/mini-harp.js
+++ bin/mini-harp.js
@@ -8,4 +8,11 @@ var miniharp = require('../')
 port = port ? port : 4000;
 
 console.log("Starting mini-harp on http://localhost:"+port);
+app.use(function(request, response, next) {
+			if (request.url == "/current-time") {
+				response.end(new Date().toISOString()+"\n");
+			} else {
+				response.end("Cannot Get "+request.url+"\n");
+			}
+		});
 app.listen(port);

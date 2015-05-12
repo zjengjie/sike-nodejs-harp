3023d478997cd6500a3dd0232f706f280da55d2c
Zeng Jie <zjengjie@gmail.com>
2015-05-13 00:59:15 +0800

    modify

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ var connect = require('connect');
 var serveStatic = require('serve-static');
 module.exports = function(root) {
 	root = root ? root : process.cwd();
-	var app = connect();
-	app.use(serveStatic(root));
-	return app;
+	return connect()
+		.use(serveStatic(root));
 }
